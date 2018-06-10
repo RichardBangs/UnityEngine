61f99479cf4ac038fb5547039c4b67e4bd9a801b
richard.bangs <rbangs@gmail.com>
2018-06-10 17:10:40 +0300

    First commit. Simple code project structure.

---
+++ Code/Game/BoatGame.Game/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Supercell.BoatGame
+{
+    public class Class1 : MonoBehaviour
+    {
+    }
+}
